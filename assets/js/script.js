3703b0da8e4a56095bb1f1772f904587ddd30dd8
PierTwo <105090994+PierTwo@users.noreply.github.com>
2022-07-17 12:54:46 -0500

    Added functionality to time blocks to change color based on the time and a margin on the bottom

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,46 @@
-var currentTime = moment();
+$(document).ready(function () {
+    var currentTime = moment();
 
-function dayTime() {
-    if (currentTime.hour() >= 6 && currentTime.hour() < 12) {
-        $("#currentDay").text(currentTime.format("[Good Morning! Today is] dddd MMMM Do[. The time is] hh:mm:ss A[.]"));
-    } else if (currentTime.hour() >= 12 && currentTime.hour() < 18) {
-        $("#currentDay").text(currentTime.format("[Good Afternoon! Today is] dddd MMMM Do[. The time is] hh:mm:ss A[.]"));
-    } else if (currentTime.hour() >= 18 && currentTime.hour() < 24) {
-        $("#currentDay").text(currentTime.format("[Good Evening! Today is] dddd MMMM Do[. The time is] hh:mm:ss A[.]"));
-    } else {
-        $("#currentDay").text(currentTime.format("[Today is] dddd MMMM Do[. The time is] hh:mm:ss A[.]"));
+    function dayTime() {
+        if (currentTime.hour() >= 6 && currentTime.hour() < 12) {
+            $("#currentDay").text(currentTime.format("[Good Morning! Today is] dddd MMMM Do[. The time is] hh:mm:ss A[.]"));
+        } else if (currentTime.hour() >= 12 && currentTime.hour() < 18) {
+            $("#currentDay").text(currentTime.format("[Good Afternoon! Today is] dddd MMMM Do[. The time is] hh:mm:ss A[.]"));
+        } else if (currentTime.hour() >= 18 && currentTime.hour() < 24) {
+            $("#currentDay").text(currentTime.format("[Good Evening! Today is] dddd MMMM Do[. The time is] hh:mm:ss A[.]"));
+        } else {
+            $("#currentDay").text(currentTime.format("[Today is] dddd MMMM Do[. The time is] hh:mm:ss A[.]"));
+        };
     };
-};
 
-setInterval(() => {
-    currentTime = moment();
+    function timeBlock() {
+        var currentHour = currentTime.hour();
+
+        $(".time-block").each(function () {
+            var hourId = parseInt($(this).prop("id"), 10);
+
+            if (hourId > currentHour) {
+                $(this).addClass("future");
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+            } else if (hourId < currentHour) {
+                $(this).addClass("past");
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+            } else {
+                $(this).addClass("present")
+                $(this).removeClass("future");
+                $(this).removeClass("past");
+            };
+        });
+    };
+
+    setInterval(() => {
+        currentTime = moment();
+        dayTime();
+        timeBlock();
+    }, 1000);
+
     dayTime();
-}, 1000);
-
-dayTime();
+    timeBlock();
+});
